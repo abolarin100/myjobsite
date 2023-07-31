d718c215b35c1079d499ddded81484a7db7b76d7
abolarin100 <abolarin100@gmail.com>
2023-07-31 15:49:55 -0700

    new commit

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -6,7 +6,7 @@ const RootLayout = () => {
         <div className="root-layout">
             <header>
                 <nav>
-                    <h1>MyJobSite</h1>
+                    <h1>JobSite</h1>
                     <NavLink to="/">Home</NavLink>
                     <NavLink to="about">About</NavLink>
                     <NavLink to="help">Help</NavLink>

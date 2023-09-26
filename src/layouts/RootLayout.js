dc4afe70b9087f28034463e849d21c4d981361a8
abolarin100 <abolarin100@gmail.com>
2023-09-26 12:09:13 -0700

    new

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 
 const RootLayout = () => {
+    const [menuVisible, setMenuVisible] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    };
+
     return (
         <div className="root-layout">
-            <header>
-                <nav>
-                    <h1>JobSite</h1>
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="about">About</NavLink>
-                    <NavLink to="help">Help</NavLink>
-                    <NavLink to="careers">Careers</NavLink>
+            <header className="header">
+                <nav className="navbar">
+                    <div className="logo">
+                        <h1>JobSite</h1>
+                    </div>
+                    <div className={`nav-links ${menuVisible ? 'show' : ''}`}>
+                        <NavLink to="/">Home</NavLink>
+                        <NavLink to="about">About</NavLink>
+                        <NavLink to="help">Help</NavLink>
+                        <NavLink to="careers">Careers</NavLink>
+                    </div>
+
+                    <div className="menu-toggle" onClick={toggleMenu}>&#9776;</div>
+
+
                 </nav>
             </header>
 
@@ -19,6 +33,6 @@ const RootLayout = () => {
             </main>
         </div>
     );
-}
+};
 
 export default RootLayout;
